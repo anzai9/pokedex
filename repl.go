90b57d222c8e3024763651c67d9acb45bb1b6ea3
Chien-An Wang <jiananwang1209@gmail.com>
2024-05-13 07:45:29 +0800

    feat: add inspect caught pokemon

--- repl.go
+++ repl.go
@@ -54,6 +54,11 @@ func getCommands() map[string]cliCommand {
 			description: "Catch a pokemon",
 			callback:    commandCatch,
 		},
+		"inspect": {
+			name:        "inspect",
+			description: "Inspect a caught pokemon",
+			callback:    commandInspect,
+		},
 	}
 }
 
